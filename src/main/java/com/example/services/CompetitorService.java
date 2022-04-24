43718a53cff0a3547b16db4862519dd4a0d16d0c
Andres Guevara <andres-guevara1@upc.edu.co>
2022-04-24 16:56:22 -0500

    Carga inicial

---
+++ src/main/java/com/example/services/CompetitorService.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.services;
+
+import com.example.PersistenceManager;
+import com.example.models.Competitor;
+import com.example.models.CompetitorDTO;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Mauricio
+ */
+@Path("/competitors")
+@Produces(MediaType.APPLICATION_JSON)
+public class CompetitorService {
+
+    @PersistenceContext(unitName = "CompetitorsPU")
+    EntityManager entityManager;
+
+    @PostConstruct
+    public void init() {
+        try {
+            entityManager = PersistenceManager.getInstance().getEntityManagerFactory().createEntityManager();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        Query q = entityManager.createQuery("SELECT u FROM Competitor u ORDER BY u.surname ASC");
+        List<Competitor> competitors = q.getResultList();
+        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitors).build();
+    }
+
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createCompetitor(CompetitorDTO competitor) {
+
+        Competitor competitorTmp = new Competitor();
+        competitorTmp.setAddress(competitor.getAddress());
+        competitorTmp.setAge(competitor.getAge());
+        competitorTmp.setCellphone(competitor.getCellphone());
+        competitorTmp.setCity(competitor.getCity());
+        competitorTmp.setCountry(competitor.getCountry());
+        competitorTmp.setName(competitor.getName());
+        competitorTmp.setSurname(competitor.getSurname());
+        competitorTmp.setTelephone(competitor.getTelephone());
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(competitor);
+        entityManager.getTransaction().commit();
+        entityManager.refresh(competitorTmp);
+
+        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitorTmp).build();
+    }
+
+}
